97aa7c9aba8edc6fe102071f2aceb6b6837c283d
SorenRemboll <soren.remboll@gmail.com>
2022-08-22 22:13:29 +0200

    DB Dump

--- src/routes/apis/pokemons/update.js
+++ src/routes/apis/pokemons/update.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 export const GET = async () => {
    
-    createAbilityEntry(1)
+    createAbilityEntry(803)
     
 	return {
 		status: 200,
@@ -19,32 +19,26 @@ const createAbilityEntry = async(ID_TO_CHECK) => {
         console.log('Done');
         return
     }
-    const conn = await fetch('https://pokeapi.co/api/v2/pokemon/'+ID_TO_CHECK);
+    const conn = await fetch('https://pokeapi.co/api/v2/pokemon-species/'+ID_TO_CHECK);
     const data = await conn.json();
-    console.log(data);
-    let checkCounter = 0;
-    for (let i = 0; i < data.stats.length; i++) {
-        const element = data.stats[i];
-        const insertedAbility = await prisma.pokemon.update({
-            where:{
-                id:ID_TO_CHECK
-            },
-            data:{
-                stats:{
-                    createMany:{
-                        data:[{name:element.stat.name,value:element.base_stat}]
-                    }
-                }
-            }
-        });
-        console.log(insertedAbility);
-        if(insertedAbility){
-            
-            checkCounter++;
-        }
+
+    let description = data.flavor_text_entries.find(e => e.language.name == 'en' && e.version.name == 'sword')?.flavor_text;
+    if (!description) {
+        description = data.flavor_text_entries.find(e => e.language.name == 'en' && e.version.name == 'ultra-sun').flavor_text;
     }
-    console.log(checkCounter,data.stats.length);
-    if (checkCounter == data.stats.length) {
+    const pkmnUpdate = await prisma.pokemon.update({
+        where:{
+            id:ID_TO_CHECK
+        },
+        data:{
+            species:data.genera.find(e=>e.language.name == 'en').genus,
+            description: description,
+
+        }
+    });
+    console.log(pkmnUpdate);
+    
+    if (pkmnUpdate) {
         createAbilityEntry(ID_TO_CHECK + 1);
     } 
 }
